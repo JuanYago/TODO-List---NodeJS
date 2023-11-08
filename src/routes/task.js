bd5f1a480521ef47ca80ab06661ca828001af26b
Juan Yago <juanyago25@gmail.com>
2023-11-08 14:33:45 -0300

    first commit

--- src/routes/task.js
+++ src/routes/task.js
@@ -14,4 +14,20 @@ checklistDependentRoute.get('/:id/tasks/new', async(req, res) => {
     }
 })
 
+
+checklistDependentRoute.post('/:id/tasks', async(req, res) => {
+   let {name} = req.body.task;
+   let task = new Task({name, checklist: req.params.id})
+    try {
+        await task.save()
+        let checklist = await Checklist.findById(req.params.id)
+       
+        checklist.tasks.push(task)
+        await checklist.save()
+        res.redirect(`/checklists/${req.params.id}`)
+    } catch (error) {
+        let errors = error.errors
+        res.status(422).render('tasks/new', { task: {...task, errors}, checklistId: req.params.id })
+}})
+
 module.exports = { checklistDependent :checklistDependentRoute}
